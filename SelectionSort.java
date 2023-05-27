572322f085428b83f24b7eef20debb27b5ebf468
Samiksha Pawar <89374047+Samikshap123@users.noreply.github.com>
2023-05-27 18:40:41 +0530

    Create SelectionSort.java

---
+++ SelectionSort.java
@@ -0,0 +1,25 @@
+public class SelectionSortGreedy {
+
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j; // find the index of the smallest element
+                }
+            }
+            // swap the smallest element with the element at the beginning of the unsorted part
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {5, 2, 8, 1, 3};
+        System.out.println("Original array: " + Arrays.toString(arr));
+        selectionSort(arr);
+        System.out.println("Sorted array: " + Arrays.toString(arr));
+    }
+}
