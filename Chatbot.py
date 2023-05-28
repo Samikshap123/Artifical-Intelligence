b2d80417f9bd481f7932a7067e84cf48e51158e3
Samiksha Pawar <89374047+Samikshap123@users.noreply.github.com>
2023-05-28 22:14:49 +0530

    Update Chatbot.py

--- Chatbot.py
+++ Chatbot.py
@@ -1,53 +1,27 @@
-#It is a Hospital Enquiry desk chatbok 
-
 import tkinter as tk
 import nltk
 from nltk.chat.util import Chat, reflections
 
-# Define a dictionary of patterns and responses for the chatbot
-patterns = [
-    (r"hi|hello|hey", ["Hello!", "Hi there!"]),
-    (r"what is your name?", ["My name is Hospital Enquiry Bot.", "I'm Hospital Enquiry Bot."]),
-    (r"what services do you offer?", ["We offer a range of medical services. Please specify your enquiry."]),
-    (r"(.*) (hours|open|close)", ["We are open 24/7. How can we assist you?"]),
-    (r"(.*) (appointment|schedule|book)", ["To book an appointment, please call our reception desk at 123-456-7890."]),
-    (r"(.*) (emergency|urgent)", ["For emergencies, please call 911 or go to the nearest emergency room."]),
-    (r"(.*) (payment|insurance|cost)", ["Please contact our billing department at 555-555-5555 for any questions related to payments or insurance."]),
-    (r"(.*)", ["Thank you for your enquiry. We will get back to you as soon as possible."])
+pattern = [
+    (r"hi|hello|hey", ["Hello"]),
+    (r"Your name", ["My name is Hospital Enquiry Bot.","I'm Hosptital assistant"]),
+    (r"What services do you offer? ", ["We offer a range of medical services. Please specify your enquiry."]),
+    (r"(.*) (hour|open|close)",["We are open 24/7. How can we assist you"]),
+    (r"(.*) (appointment|schedule|book)",["To book an appointment, please call our reception supreet birajdar at 123-456-789 "]),
+    (r"(.*) (emergency|noraml checking)",["For energency please call at 911 or go to nearest hospital"]),
+    (r"(.*) (payment|insurance|cost)",["Please conatct our billing department at 111-111-222-222 for any question related to payment"]),
+    
+    (r"(.*) (thanks|thank you)",["thank you for your Enquiry."])
 ]
 
-# Create a chatbot object using the defined patterns and responses
-chatbot = Chat(patterns, reflections)
+#chatbot = Chat(pattern, reflections)
+#chatbot.converse()
 
-# Define a function to handle button click events
-def button_click():
-    # Get user input from the text entry widget
-    user_input = entry.get()
-    # Clear the text entry widget
-    entry.delete(0, tk.END)
-    # Generate a response to the user's input using the chatbot object
-    response = chatbot.respond(user_input)
-    # Display the response in the chat history widget
-    history.config(state=tk.NORMAL)
-    history.insert(tk.END, "You: " + user_input + "\n")
-    history.insert(tk.END, "Bot: " + response + "\n")
-    history.config(state=tk.DISABLED)
 
-# Create the main window
-root = tk.Tk()
-root.title("Hospital Enquiry Bot")
+def chat():
+    print("Hello, I am Hospital Enquiry chatbot, Hope you are well and fine..")
+    chat = Chat(pattern,reflections)
+    chat.converse()
 
-# Create the chat history widget
-history = tk.Text(root, state=tk.DISABLED)
-history.pack(fill=tk.BOTH, expand=True)
-
-# Create the text entry widget
-entry = tk.Entry(root)
-entry.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
-
-# Create the send button
-button = tk.Button(root, text="Send", command=button_click)
-button.pack(side=tk.RIGHT)
-
-# Start the main event loop
-root.mainloop()
+if __name__ == "__main__":
+    chat()
